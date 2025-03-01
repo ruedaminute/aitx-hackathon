fc9d206b7c802f61307fc24f86526a462e1dd910
Michelle Rueda <ruedaminute@gmail.com>
2025-02-28 19:37:30 -0600

    Initial Commit

---
+++ AppWarmr/AppWarmrApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppWarmrApp.swift
+//  AppWarmr
+//
+//  Created by Michelle Rueda on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct AppWarmrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
